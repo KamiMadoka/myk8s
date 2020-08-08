166a20d5ec942773712a94e40aabc8b658d3d332
Madoka <KamiMadoka@outlook.com>
2020-08-08 21:35:35 +0800

    front 引入react

--- front/src/index.js
+++ front/src/index.js
@@ -1,3 +1,15 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+function App() {
+    return <div>my react</div>
+}
+
+ReactDom.render(
+    <App />,
+    document.getElementById('root')
+);
+
 function component() {
     var element = document.createElement('div');
     var btn = document.createElement('button');
--- front/webpack.config.js
+++ front/webpack.config.js
@@ -20,6 +20,26 @@ module.exports = {
             template: './src/index.html'
         })
     ],
+    module: {
+        rules: [
+            {
+                test: /\.js|jsx$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env',
+                            '@babel/preset-react',
+                            {
+                                'plugins': ['@babel/plugin-proposal-class-properties']
+                            }
+                        ]
+                    }
+                }
+            }
+        ]
+    },
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
